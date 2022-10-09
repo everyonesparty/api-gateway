08ae2e2c8627e83b1d44b2eb33331cb468c9aee0
rnjstjdgh <gshgsh0831@daum.net>
2022-10-09 15:55:05 +0900

    테스트 프로파일 분리 & JwtUtil 리팩토링

--- src/main/java/everyonesparty/apigateway/exception/error/JwtError.java
+++ src/main/java/everyonesparty/apigateway/exception/error/JwtError.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum JwtError implements RestError {
 
-    FORBIDDEN_JWT_TOKEN(HttpStatus.FORBIDDEN, "JWT: 해당 리소스에 접근할 권한이 없습니다.(토큰은 유효하지만 권한 없음)"),
+    FORBIDDEN_JWT_TOKEN(HttpStatus.FORBIDDEN, "JWT: 해당 리소스에 접근할 권한이 없습니다.(토큰은 유효하지만 권한 없음)"),  // TODO: 추후 Role 에 따른 인가작업이 생기면 도입될 에러
     NOT_EXIST_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "JWT: 토큰 없는 요청 입니다."),
     INVALID_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "JWT: 유효하지 않은 토큰 입니다.");
 
