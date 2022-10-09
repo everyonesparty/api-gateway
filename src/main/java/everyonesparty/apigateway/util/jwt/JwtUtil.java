7cd524ad29a44254db56ca2135e9d4d568fb2baf
rnjstjdgh <gshgsh0831@daum.net>
2022-10-09 15:55:05 +0900

    테스트 프로파일 분리 & JwtUtil 리팩토링

--- src/main/java/everyonesparty/apigateway/util/jwt/JwtUtil.java
+++ src/main/java/everyonesparty/apigateway/util/jwt/JwtUtil.java
@@ -1,6 +1,7 @@
 package everyonesparty.apigateway.util.jwt;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
@@ -8,10 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.Date;
-import java.util.Set;
+import java.util.*;
 
 @Slf4j
 @Component
@@ -28,6 +26,10 @@ public class JwtUtil {
         secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
     }
 
+    /***
+     * jwt token 생성을 front-server 로 위임
+     */
+    @Deprecated
     public String createToken(String userPk, Set<UserRole> roles) {
         Claims claims = Jwts.claims().setSubject(userPk);
         claims.put("roles", new ArrayList<>(roles));
@@ -35,68 +37,61 @@ public class JwtUtil {
         return Jwts.builder()
                 .setClaims(claims) // 데이터
                 .setIssuedAt(now) // 토큰 발행일자
-                .setExpiration(new Date(now.getTime() + 3600000)) // set Expire Time
+                .setExpiration(new Date(now.getTime() + tokenValidMilisecond)) // 토큰만료일자
                 .signWith(SignatureAlgorithm.HS256, secretKey) // 암호화 알고리즘, secret값 세팅
                 .compact();
     }
 
-
     /***
-     * > 디비 안거치는 단순 jwt 검증
+     * > 디비 안거치는 단순 jwt 검증 -> secretKey 로 정상적으로 까지기만 하면 통과되는 인증 필터
      * > TODO: 필요 시 디비에 저장된 사용자 아이디와 토큰에 포함된 사용자 아이디 검증하는 별도 기능 추가
-     * @param jwt
-     * @return
      */
     public boolean IsValidJwt(String jwt) {
-        String subject = null;
-        try{
-            subject = Jwts.parser().setSigningKey(secretKey)
-                    .parseClaimsJws(jwt).getBody()
-                    .getSubject();
-        }catch (Exception ex){
-            log.error("jwt parsing error. {}", ex.getMessage());
-            return false;
-        }
-
-        if(subject == null || subject.isEmpty()){
-            log.error("subject is invalid. subject: {}", subject);
-            return false;
-        }
-
-        log.info("extracted subject is {}", subject);
-        return true;
+        return parseJwt(jwt)
+                .flatMap(this::getSubject)
+                .filter(subject -> !subject.isEmpty())
+                .map(subject -> {
+                    log.info("extracted subject is {}", subject);
+                    return true;
+                }).orElse(false);
     }
-
 
     public boolean hasKaKaoUserRole(String jwt){
         return this.hasValidRole(jwt, UserRole.KAKAO_USER);
     }
 
-
-
     /***
      * > jwt 에 userRole 이 있는지?
-     * @param jwt
-     * @param userRole
-     * @return
      */
     private boolean hasValidRole(String jwt, UserRole userRole) {
-        boolean isValid = true;
-        ArrayList<String> userRoles = null;
-        try{
-            userRoles = (ArrayList<String>) Jwts.parser().setSigningKey(secretKey)
-                    .parseClaimsJws(jwt).getBody()
-                    .get("roles");
-        }catch (Exception ex){
-            log.error("jwt parsing error. {}", ex.getMessage());
-            isValid = false;
-        }
-
-        if(userRoles == null || userRoles.isEmpty() || !userRoles.contains(userRole.name())){
-            log.error("This jwt token does not have role {}.", userRole);
-            isValid = false;
-        }
-
-        return isValid;
+        return parseJwt(jwt)
+                .flatMap(this::getUserRoles)
+                .filter(userRoles -> !userRoles.isEmpty())
+                .filter(userRoles -> userRoles.contains(userRole.name()))
+                .map(userRoles -> {
+                    log.info("extracted userRoles is {}", userRoles.toString());
+                    return true;
+                })
+                .orElse(false);
     }
+
+    private Optional<Claims> parseJwt(String jwt) {
+        try {
+            return Optional.ofNullable(Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(jwt).getBody());
+        } catch (Exception ex) {
+            log.error("jwt parsing error. {}", ex.getMessage(), ex);
+            return Optional.empty();
+        }
+    }
+
+    private Optional<String> getSubject(Claims parsedJwt) {
+        return Optional.ofNullable(parsedJwt.getSubject());
+    }
+
+    private Optional<ArrayList<String>> getUserRoles(Claims parsedJwt) {
+        return Optional.ofNullable((ArrayList<String>) parsedJwt.get("roles"));
+    }
+
 }
